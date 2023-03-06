6634259f56f34999d03c0f00564543ddda7d03e6
Marwabrian <marwabrian7@gmail.com>
2023-03-06 22:00:19 +0300

    different changes

--- mysite/delight_Cakehouse/models.py
+++ mysite/delight_Cakehouse/models.py
@@ -4,14 +4,16 @@ from django.utils import timezone
 
 
 class Caakes(models.Model):
+    Image = models.ImageField(default='default.jpg', upload_to='home/marwa254/Desktop/DELIGHT/mysite/delight_Cakehouse/images')
     cake_type = models.TextField(blank=True, null=True)
     availability = models.IntegerField(blank=True, null=True)
     Best_Before = models.DateTimeField(default=timezone.now)
     in_stock = models.BooleanField('in stock')
 
-"""    def __str__(self):
-        return self.cake_type()  """
 
+"""     def __str__(self): 
+        return self.cake_type()  
+ """
 class Cookies(models.Model):
     cookie_flavor = models.CharField(max_length=100)
     available_stock = models.BooleanField()
