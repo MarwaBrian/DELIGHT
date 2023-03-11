f5d12f7c65499f1ec46aa3c8d81902b1c8091dca
Marwabrian <marwabrian7@gmail.com>
2023-03-12 01:11:16 +0300

    Updated Tables

--- mysite/delight_Cakehouse/models.py
+++ mysite/delight_Cakehouse/models.py
@@ -7,7 +7,7 @@ class Caakes(models.Model):
     Image = models.ImageField(default='default.jpg', upload_to='home/marwa254/Desktop/DELIGHT/mysite/delight_Cakehouse/images')
     cake_type = models.TextField(blank=True, null=True)
     availability = models.IntegerField(blank=True, null=True)
-    Best_Before = models.DateTimeField(default=timezone.now)
+    Best_Before = models.DateField(default=timezone.now)
     in_stock = models.BooleanField('in stock')
 
 
